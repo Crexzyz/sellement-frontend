84e293f9c2025cb076047478588f7808f0437b44
Steven Fernández <vsteven25@gmail.com>
2022-01-12 20:38:33 -0600

    Send post and save token when clicking login button

--- src/app/users/components/login/login.component.ts
+++ src/app/users/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../../services/authentication.service'
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -10,23 +12,48 @@ export class LoginComponent implements OnInit {
   hidePassword: boolean = true;
   loginForm: FormGroup = {} as FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  private ERROR_MESSAGES: {[char: string]: string} = {
+    required: "This field is required",
+    email: "Invalid email",
+  };
+
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthenticationService,
+              private localStorageService: LocalStorageService) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       "email": [null, [Validators.required, Validators.email]],
       "password": [null, [Validators.required]]
     })
+  }
 
-    this.loginForm.valueChanges.subscribe(console.log)
+  async login(): Promise<void> {
+    this.loginForm.markAllAsTouched();
+    let email = this.loginForm.get("email");
+
+    if(email == null || email.errors)
+      return;
+
+    let password = this.loginForm.get("password");
+
+    if(password == null || password.errors)
+      return;
+
+    const data = await this.authService.login(email.value, password.value);
+    const token = data["access_token"]
+    if(token == undefined)
+      return
+    
+    this.localStorageService.set("access_token", token)
   }
 
   getError(fieldName: string, errorName: string): string {
     let field = this.loginForm.get(fieldName)
     if(field == null)
       return ""
-
-    return field.hasError(errorName) ? "Error" : ""
+    
+    return field.hasError(errorName) ? this.ERROR_MESSAGES[errorName] : ""
   }
 
 }
--- src/app/users/services/authentication.service.ts
+++ src/app/users/services/authentication.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { firstValueFrom, lastValueFrom, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +10,16 @@ export class AuthenticationService {
 
   constructor(private client: HttpClient) { }
 
-  public login(email: string, password: string): Observable<any> {
-    // TODO: Add token to localstorage if successful
+  public async login(email: string, password: string): Promise<any> {
     const data = {"email": email, "password": password};
-    return this.client.post(`${this.SERVER_ADDRESS}/users/login/`, data);
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
+    const request = this.client.post(`${this.SERVER_ADDRESS}/users/login/`, JSON.stringify(data), httpOptions);
+    return await firstValueFrom(request);
   }
 
-  public logout(): Observable<any> {
+  public async logout(): Promise<any> {
     // TODO: Remove token from localstorage
     // TODO: Implement in backend
     return this.client.post(`${this.SERVER_ADDRESS}/users/logout/`, {});
