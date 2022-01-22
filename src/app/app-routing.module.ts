61b70cc518ef28b7d38c483f6d2af4a993e8466b
Steven Fernández <vsteven25@gmail.com>
2022-01-21 19:20:16 -0600

    Add empty products module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { HomeComponent } from './home/components/home/home.component';
 import { LoginComponent } from './users/components/login/login.component';
 import { LoginGuard } from './users/guards/login.guard';
 import { LoginResolver } from './users/guards/login.resolver';
+import { ProductListComponent } from './products/components/product-list/product-list.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, canActivate: [LoginGuard]},
   {path: 'login', component: LoginComponent, resolve: [LoginResolver]},
+  {path: 'products', component: ProductListComponent, canActivate: [LoginGuard]},
   // TODO: Common not found page
   // {path: '**', component: ErrorComponent},
 ]
