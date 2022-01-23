e4fb15f38cd196e31904f472e29305910c79da9b
Steven Fernández <vsteven25@gmail.com>
2022-01-22 22:45:09 -0600

    Add empty product create component

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,15 +3,31 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
+import { ProductCreateComponent } from './components/product-create/product-create.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from '../users/guards/login.guard';
+
+const routes: Routes = [
+  {
+    path: 'products', canActivate: [LoginGuard],
+    children: [
+      {path: '', component: ProductListComponent, pathMatch: 'full'},
+      {path: 'create', component: ProductCreateComponent}
+    ]
+  }
+]
+
 
 @NgModule({
   declarations: [
-    ProductListComponent
+    ProductListComponent,
+    ProductCreateComponent
   ],
   imports: [
     CommonModule,
     MatTableModule,
     MatPaginatorModule,
+    RouterModule.forChild(routes)
   ],
   exports: [
     ProductListComponent
