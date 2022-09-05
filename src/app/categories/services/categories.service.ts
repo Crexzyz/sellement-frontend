9a7f45d98e5865faef22fcb74e4d16067e879a2f
Steven Fernández <vsteven25@gmail.com>
2022-09-04 23:11:37 -0600

    Implement remaining methods in CategoriesService

--- src/app/categories/services/categories.service.ts
+++ src/app/categories/services/categories.service.ts
@@ -1,7 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom, Observable } from 'rxjs';
+import { FormFieldFactory } from 'src/app/core/models/form-fields/form-field-factory';
 import { BaseRestService } from 'src/app/core/services/base-rest.service';
 import { Category } from '../models/category.model';
+
+// TODO: This code is almost the same as in products service, template methods
+// may be useful
 
 @Injectable({
   providedIn: 'root'
@@ -11,20 +16,44 @@ export class CategoriesService extends BaseRestService<Category> {
     super(httpClient, "categories");
   }
 
-  get(object: Category): Promise<Category> {
-    throw new Error('Method not implemented.');
+  async get(object: Category): Promise<Category> {
+    let request = this._httpClient.get<Category>(`${this.endpoint}/${object.name}/`);
+    let data = await lastValueFrom(request);
+    return data;
   }
-  create(object: Category): Promise<Category> {
-    throw new Error('Method not implemented.');
+
+  async create(object: Category): Promise<Category> {
+    const data = object.toJson();
+    let request = this._httpClient.post<Category>(`${this.endpoint}/`, data);
+    let response = await lastValueFrom(request);
+    return response;
   }
-  update(object: Category): Promise<Category> {
-    throw new Error('Method not implemented.');
+
+  async update(object: Category): Promise<Category> {
+    const data = object.toJson();
+    let request = this._httpClient.put<Category>(`${this.endpoint}/${object.name}/`, data);
+    let response = await lastValueFrom(request);
+    return response;
   }
-  delete(object: Category): Promise<Category> {
-    throw new Error('Method not implemented.');
+
+  async delete(object: Category): Promise<Category> {
+    const data = object.toJson();
+    let request = this._httpClient.delete<Category>(`${this.endpoint}/${object.name}/`, data);
+    let response = await lastValueFrom(request);
+    return response;
   }
-  form(object?: Category | undefined): Promise<any> {
-    throw new Error('Method not implemented.');
+
+  async form(category?: Category | undefined): Promise<any> {
+    let request: Observable<any>;
+
+    if(category === undefined) {
+      request = this._httpClient.get<any>(`${this.endpoint}/form/`);
+    } else {
+      request = this._httpClient.get<any>(`${this.endpoint}/${category.name}/form/`);
+    }
+
+    let response = await lastValueFrom(request);
+    return FormFieldFactory.createFields(response);
   }
 
 }
